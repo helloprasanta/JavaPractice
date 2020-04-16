4736689e70208565d27a40ad5cbb7ed56aa2d546
PRASANTA KUMAR PARIDA <prasanta.parida@thinktalentindia.com>
2020-04-16 19:08:32 +0530

    Java Stream Api Practice

---
+++ src/main/java/com/streamapi/StreamApiTest.java
@@ -0,0 +1,19 @@
+package com.streamapi;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamApiTest {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+        arrayList.add(0);
+        arrayList.add(5);
+        arrayList.add(10);
+        arrayList.add(15);
+        arrayList.add(20);
+        System.out.println("Old Array List"+arrayList);
+        List<Integer> newArrayList=arrayList.stream().filter(i -> i%2==0).collect(Collectors.toList());
+        System.out.println("newArrayList"+newArrayList);
+    }
+}
